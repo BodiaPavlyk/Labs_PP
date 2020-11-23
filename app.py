7d0921934850e04c8acad07bee947f36b2c18956
BodiaPavlyk <bogdan2002mih@gmail.com>
2020-11-23 13:57:27 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,jsonify,request,render_template
+app = Flask(__name__)
+
+
+@app.route('/Bodia')
+def hello_world():
+    test = request.args.get('test', '')
+    return "test = " + test
+
+
+if __name__ == '__main__':
+    app.run()
